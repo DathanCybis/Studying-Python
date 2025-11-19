980d1ac4235f00a211b5713ff99bbc1bf0cd23b0
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-11-19 15:20:19 -0300

    Create manipulandoStringEx.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/manipulandoStringEx.py
@@ -0,0 +1,17 @@
+#1
+frase = "Python é uma linguagem de programação poderosa e versátil."
+
+#2
+print(f"Tamanho da frase: {len(frase)}")
+
+#3
+palavra = frase.split()[0]
+print(f"Primeira palavra da frase: {palavra}")
+
+#4
+frase_maiscula = frase.upper()
+print(f"Frase em maiúsculas: {frase_maiscula}")
+
+#5
+frase_substituida = frase.replace("poderosa", "incrível")
+print(f"Frase com substituição: {frase_substituida}")
