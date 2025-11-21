97d316ae3fbcd579b11b1ad1a767e16e8ddca6ad
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-11-21 12:46:21 -0300

    Create variávelLocal.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/variávelLocal.py
@@ -0,0 +1,12 @@
+contador = 0 # <<<<<<<<<<<<<<<<
+
+def implementarContador():
+    contador = 0 # >>>>>>>>>>>>>>>
+
+    contador += 1 # >>>>>>>>>>>>>>>
+
+    print(contador) # >>>>>>>>>>>>>>>
+
+print(contador) # <<<<<<<<<<<<<<<<
+
+implementarContador() # >>>>>>>>>>>>>>>
