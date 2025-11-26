45b84a19480a709d7676817f3cdf03f0e2bac63c
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-11-26 11:16:41 -0300

    Create tuplasFor.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/tuplasFor.py
@@ -0,0 +1,10 @@
+vendas = [(100, 85), (100, 115), (105, 110)]
+
+for vendas_A, vendas_B in vendas:
+    print("Vendas de A:", vendas_A, end=", ")
+    print("Vendas de B:",vendas_B)
+
+# Saída:
+# Vendas de A: 100, Vendas de B: 85
+# Vendas de A: 100, Vendas de B: 115
+# Vendas de A: 105, Vendas de B: 110
