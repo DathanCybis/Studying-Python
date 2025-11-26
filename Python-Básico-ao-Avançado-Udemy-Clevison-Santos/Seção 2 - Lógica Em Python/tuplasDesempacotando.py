8d3e9796384303da60ed4ee32631addabffc3bf6
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-11-26 11:01:49 -0300

    Create tuplasDesempacotando.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/tuplasDesempacotando.py
@@ -0,0 +1,9 @@
+estudante_info = ("João", 20, 85.5)
+
+# Desempacotamento 
+
+nome, idade, media = estudante_info
+
+print(f"O nome do aluno é: {nome}") # Saída: O nome do aluno é: João
+print(f"A idade do aluno é {idade}") # Saída: A idade do aluno é 20
+print(f"A média do aluno é: {media}") # Saída: A média do aluno é: 85.5
