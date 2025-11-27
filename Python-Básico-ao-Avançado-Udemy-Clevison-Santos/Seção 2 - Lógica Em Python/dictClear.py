94c3eb835eafb9ea70fc6b0d81de001f5eadb61c
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-11-27 17:50:00 -0300

    Create dictClear.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/dictClear.py
@@ -0,0 +1,11 @@
+livros = {
+    "titílo": "livro",
+    "autor": "alguém",
+    "lancamento": "algum dia"
+}
+
+print(livros) # {'titílo': 'livro', 'autor': 'alguém', 'lancamento': 'algum dia'}
+
+livros.clear()
+
+print(livros) # {}
