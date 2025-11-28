80fdd0baa35cebd192a8097b75bb0b3b12316308
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-11-28 19:26:39 -0300

    Create dictComprehension(c).py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/dictComprehension(c).py
@@ -0,0 +1,5 @@
+palavras = ["Python", "Compreensão", "Dicionário"]
+
+comprimentos = {palavra: len(palavra) for palavra in palavras}
+
+print(comprimentos) # {'Python': 6, 'Compreensão': 11, 'Dicionário': 10}
