55f9a6e46c1a2a3f5539ca629b871ebbfce8bf40
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-11-28 18:40:56 -0300

    Create dictFromKeys.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/dictFromKeys.py
@@ -0,0 +1,5 @@
+chaves = ["Título", "Autor", "Sinopse"]
+
+novo_livro = dict.fromkeys(chaves, "Desconhecido")
+
+print(novo_livro) # {'Título': 'Desconhecido', 'Autor': 'Desconhecido', 'Sinopse': 'Desconhecido'}
