65d26ae674dc327d4c500027df74fc5cd7808a9d
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-11-29 23:17:18 -0300

    Update dictFunction.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/dictFunction.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/dictFunction.py
@@ -8,4 +8,4 @@ def criar_perfil(nome, idade, email):
 
 novo_usuario = criar_perfil("Paulo", 32, "paulo@email.com")
 
-print(novo_usuario) # {'nome': 'Paulo', 'idade': 32, 'email': 'paulo@email.com'}
+print(novo_usuario) # {'nome': 'Paulo', 'idade': 32, 'email': 'paulo@email.com'} 
