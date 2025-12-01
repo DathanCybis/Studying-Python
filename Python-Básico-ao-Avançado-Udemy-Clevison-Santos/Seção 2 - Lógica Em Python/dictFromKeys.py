8c22d20fc82375c469535fd68aad6b5658bb229b
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-11-30 23:52:19 -0300

    Update dictFromKeys.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/dictFromKeys.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/dictFromKeys.py
@@ -2,4 +2,4 @@ chaves = ["Título", "Autor", "Sinopse"]
 
 novo_livro = dict.fromkeys(chaves, "Desconhecido")
 
-print(novo_livro) # {'Título': 'Desconhecido', 'Autor': 'Desconhecido', 'Sinopse': 'Desconhecido'}
+print(novo_livro) # {'Título': 'Desconhecido', 'Autor': 'Desconhecido', 'Sinopse': 'Desconhecido'} 
