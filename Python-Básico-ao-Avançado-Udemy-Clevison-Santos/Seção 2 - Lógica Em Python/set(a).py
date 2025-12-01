ec96e88ad2fc82c322d45f5275bdc6fe647d2105
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-01 18:03:30 -0300

    Update set(a).py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/set(a).py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/set(a).py
@@ -2,3 +2,9 @@ conjunto = {1, 2, "Python", (4, 5)}
 
 print(conjunto) # {'Python', 1, 2, (4, 5)}
 # Ordem aleatória
+
+try:
+    conjunto.add([6,7])
+except TypeError as e:
+    print(f"Erro: {e}") # Erro: unhashable type: 'list'
+# Isso acontece porque os elementos dentro de um conjunto(sets) são imutáveis
