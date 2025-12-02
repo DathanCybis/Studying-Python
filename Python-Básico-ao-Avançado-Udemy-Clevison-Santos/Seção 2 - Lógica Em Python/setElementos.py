97204400b5cef5e97e1eaddd36299b0aa94455b7
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-02 20:25:00 -0300

    Create setElementos.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/setElementos.py
@@ -0,0 +1,18 @@
+s = {1, 2, 3, 4}
+print(s) # {1, 2, 3, 4}
+
+s.add(5)
+print(s) # {1, 2, 3, 4, 5}
+
+s.remove(5)
+print(s) # {1, 2, 3, 4}
+
+s.discard(4)
+print(s) # {1, 2, 3}
+
+elemento_removido = s.pop()
+print(elemento_removido) # 1
+print(s) # {2, 3}
+
+s.clear()
+print(s) # set()
