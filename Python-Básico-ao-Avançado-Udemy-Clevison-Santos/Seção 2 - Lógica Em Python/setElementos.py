bc7400791aec7bb9fd9358b358cf736b9064b719
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-02 20:27:06 -0300

    Update setElementos.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/setElementos.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/setElementos.py
@@ -4,7 +4,7 @@ print(s) # {1, 2, 3, 4}
 s.add(5)
 print(s) # {1, 2, 3, 4, 5}
 
-s.remove(5)
+s.remove(5) # Gera erros se não houver o número a ser excluído
 print(s) # {1, 2, 3, 4}
 
 s.discard(4) # Não gera erros se não houver o número a ser excluído
