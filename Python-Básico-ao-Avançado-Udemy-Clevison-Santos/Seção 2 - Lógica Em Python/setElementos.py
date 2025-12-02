e2148410c228010241a8302f89c1216b5e79c74b
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-02 20:26:52 -0300

    Update setElementos.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/setElementos.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/setElementos.py
@@ -7,7 +7,7 @@ print(s) # {1, 2, 3, 4, 5}
 s.remove(5)
 print(s) # {1, 2, 3, 4}
 
-s.discard(4)
+s.discard(4) # Não gera erros se não houver o número a ser excluído
 print(s) # {1, 2, 3}
 
 elemento_removido = s.pop()
