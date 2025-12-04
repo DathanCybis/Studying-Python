b0e46fc3af240c703450596dba46efb12e35b1c6
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-04 16:41:48 -0300

    Update matrixExUrna.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/matrixExUrna.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/matrixExUrna.py
@@ -3,13 +3,27 @@ urna = [["Alice", 0], ["Bob", 0], ["Charlie", 0]]
 for n in range(4):
     voto = input("Digite o nome de quem quer votar (Alice, Bob ou Charlie): ")
 
-    if "Alice" in voto:
-        urna[0][1] += 1
-    elif "Bob" in voto:
-        urna[1][1] += 1
-    elif "Charlie" in voto:
-        urna[2][1] += 1
-    else:
-        print("Candidato não existe!")
+    encontrado = False
 
-print(urna)
+    for candidato in urna:
+        if candidato[0] == voto:
+            candidato[1] += 1
+            encontrado = True
+            break
+    
+    if not encontrado:
+        print("Voto nulo!")
+
+print("\nResultados:")
+
+maior = 0
+vencedor = ""
+
+for candidato in urna:
+    print(f"{candidato[0]}: {candidato[1]}")
+
+    if candidato[1] > maior:
+        maior = candidato[1]
+        vencedor = candidato[0]
+
+print(f"O vencedor é {vencedor} com {maior} votos")
