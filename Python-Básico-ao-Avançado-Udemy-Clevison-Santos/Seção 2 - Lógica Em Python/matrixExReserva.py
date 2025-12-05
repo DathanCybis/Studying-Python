a911f3df943a0e103baf7d76fdb5124163da44bc
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-05 10:43:10 -0300

    Create matrixExReserva.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/matrixExReserva.py
@@ -0,0 +1,14 @@
+matrix = [
+    ["D", "D"]*5,
+    ["D", "D"]*5,
+    ["D", "D"]*5,
+    ["D", "D"]*5
+]
+
+for linha in matrix:
+
+    for coluna in linha:
+
+        print(coluna, end=" ")
+
+    print()
