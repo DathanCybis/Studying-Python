48d2b5fabadc3bec32e90f6ef9b19047851ce661
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-05 10:50:43 -0300

    Update matrixExReserva.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/matrixExReserva.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/matrixExReserva.py
@@ -22,6 +22,25 @@ def mostrar_menu():
     print("3. Sair")
 
 
+def reservar_assento():
+    fileira = int(input("Digite o número da fileira / Linha (1-4): "))-1
+    
+
+
 def main():
     while True:
         mostrar_menu()
+
+        opc = input("\nEscolha a opção desejada: ")
+
+        if opc == "1":
+            mostrar_matrix()
+        elif opc == "2":
+            reservar_assento()
+        elif opc == "3":
+            print("\nSaindo...")
+            break
+        else:
+            print("Digite uma opção válida!")
+
+main()
