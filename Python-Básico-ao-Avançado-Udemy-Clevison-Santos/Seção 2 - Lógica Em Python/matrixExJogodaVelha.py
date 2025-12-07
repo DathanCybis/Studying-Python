1d43d70fce761544fab0e3e0e9507d8b1f31cb99
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-07 12:28:25 -0300

    Update matrixExJogodaVelha.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/matrixExJogodaVelha.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/matrixExJogodaVelha.py
@@ -15,16 +15,27 @@ def mostrar_jogo():
     print()
 
 
+def aplicar_escolha(escolha):
+    matrix[escolha[0]][escolha[2]] = ""
+
+
 def main():
     cont = 0
     while True:
+        mostrar_jogo()
         cont+=1
 
         if cont % 2 != 0:
-            escolha = int(input("Jogador 'O', escolha a linha e coluna (ex: 1 3): "))
+            escolha = input("Jogador 'O', escolha a linha e coluna (ex: 1 3): ")
+            matrix[escolha[0]][escolha[2]] = "O"
         else:
-            escolha = int(input("Jogador 'X', escolha a linha e coluna (ex: 1 3): "))
+            escolha = input("Jogador 'X', escolha a linha e coluna (ex: 1 3): ")
+            matrix[escolha[0]][escolha[2]] = "X"
         
+        aplicar_escolha(escolha)
+
+        print(escolha)
+        print()
         
 
 main()
