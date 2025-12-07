a141f69c370276b56386cb8560cf3032f8a2604c
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-07 13:42:53 -0300

    Update matrixExJogodaVelha.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/matrixExJogodaVelha.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 2 - Lógica Em Python/matrixExJogodaVelha.py
@@ -13,37 +13,13 @@ def mostrar_jogo():
         print()
         print(" ---" * 3)
     print()
-
-
-def analisar(jogador):
-    for i in range(3):
-        todas_linhas = True
-        todas_colunas = True
-
-        for j in range(3):
-            if matrix[i][j] != jogador:
-                todas_linhas = False
-
-            if matrix[j][i] != jogador:
-                todas_colunas = False
-
-    if todas_linhas or todas_colunas:
-        return True
-    
-    if matrix[0][0] == jogador and matrix[1][1] == jogador and matrix[2][2] == jogador:
-        return True
-    
-    if matrix[0][2] == jogador and matrix[1][1] == jogador and matrix[2][0] == jogador:
-        return True
-     
-    return False     
-
+ 
 
 def main():
     cont = 0
     while True:
-        mostrar_jogo()
         cont+=1
+        mostrar_jogo()
 
         if cont % 2 != 0:
             escolha = input("Jogador 'O', escolha a linha e coluna (ex: 0 2): ")
@@ -55,10 +31,6 @@ def main():
         linha = int(escolha[0])
         coluna = int(escolha[2])
         matrix[linha][coluna] = jogador
-
-        if analisar(jogador):
-            print(f"Jogador {jogador} venceu!")
-            break
 
         print()
 
