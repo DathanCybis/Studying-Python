daa6da7f31b65100600c51b1fac6a3948ead60c3
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-09 15:14:03 -0300

    Update poo-conceitosBásicos.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/poo-conceitosBásicos.py
@@ -0,0 +1,12 @@
+#1. Classes e Objetos
+class Livro:
+    
+    def __init__(self, titulo, autor, ano):
+
+        self.titulo = titulo
+        self.autor = autor
+        self.ano = ano
+
+meu_livro = Livro("1984", "George Orwell", 1949)
+
+#2. Atributos
