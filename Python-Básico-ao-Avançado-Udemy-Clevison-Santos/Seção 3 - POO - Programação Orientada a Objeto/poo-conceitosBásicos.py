99ad6976a4f4d08366b30203a3aca77db69cdcf6
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-09 15:15:28 -0300

    Update poo-conceitosBásicos.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/poo-conceitosBásicos.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/poo-conceitosBásicos.py
@@ -10,3 +10,8 @@ class Livro:
 meu_livro = Livro("1984", "George Orwell", 1949)
 
 #2. Atributos
+print(meu_livro.titulo) # 1984
+print(meu_livro.autor) # George Orwell
+print(meu_livro.ano) # 1949
+
+#3. Métodos
