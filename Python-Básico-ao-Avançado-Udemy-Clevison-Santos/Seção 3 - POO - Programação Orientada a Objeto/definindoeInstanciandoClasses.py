c402b1c5765452f794b3163485c0c89d26bbacf8
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-10 18:27:52 -0300

    Update definindoeInstanciandoClasses.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/definindoeInstanciandoClasses.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/definindoeInstanciandoClasses.py
@@ -4,3 +4,17 @@ class Carro:
         self.marca = marca
         self.modelo = modelo
         self.cor = cor
+        self.velocidade = 0
+
+
+    def acelerar(self):
+        self.velocidade += 10
+        print(f"Acelerando... Velocidade atual: {self.velocidade} km/h")
+
+    
+    def freiar(self):
+        self.velocidade -= 10
+        print(f"Freiando... Velocidade atual: {self.velocidade} km/h")
+
+
+    
