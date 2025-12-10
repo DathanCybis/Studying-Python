d9517a9599e78b23893f9494aaa0589e3756ca56
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-10 20:43:03 -0300

    Update definindoeInstanciandoClassesV2.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/definindoeInstanciandoClassesV2.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/definindoeInstanciandoClassesV2.py
@@ -22,7 +22,7 @@ class Carro:
 
 
     def exibir_info(self):
-        print(f"Marca: {self.marca}, Modelo: {self.modelo}, Cor: {self.cor}")
+        print(f"Marca: {self.marca}, Modelo: {self.modelo}, Cor: {self.cor}, Velocidade: {self.velocidade} km/h")
 
 
 lista_carros = []
@@ -39,7 +39,7 @@ def main():
     lista = []
 
     while True:
-        print(" --- MENU --- ")
+        print("\n --- MENU --- ")
         print("1. Adicionar novo carro")
         print("2. Exibir informações dos carros")
         print("3. Acelerar um carro")
@@ -51,14 +51,14 @@ def main():
         if opc == "1":
             novo_carro = adicionar_carro()
             lista_carros.append(novo_carro)
-            print("\nCarro adicionado com sucesso!\n")
+            print("\nCarro adicionado com sucesso!")
 
         elif opc == "2":
             if lista_carros:
                 for carro in lista_carros:
                     carro.exibir_info()
             else:
-                print("\nNenhum carro foi adicionado ainda.\n")
+                print("\nNenhum carro foi adicionado ainda.")
 
         elif opc == "3":
             modelo = input("\nDigite o modelo do carro que quer acelerar: ")
@@ -68,7 +68,7 @@ def main():
                     carro.acelerar()
                     break
             else:
-                print("Modelo não encontrado.\n")
+                print("Modelo não encontrado.")
 
         elif opc == "4":
             modelo = input("\nDigite o modelo do carro que quer frear: ")
@@ -78,14 +78,14 @@ def main():
                     carro.frear()
                     break
             else:
-                print("Modelo não encontrado.\n")
+                print("Modelo não encontrado.")
 
         elif opc == "5":
             print("\nFinalizando o sistema...\n")
             break
 
         else:
-            print("Digite uma opção válida!\n")
+            print("Digite uma opção válida!")
 
 
 main()
