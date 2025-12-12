a535b929b9155cc9fa6ef94353fb543fc150d0a9
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-11 22:12:52 -0300

    Update definindoeAcessandoAtributos.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/definindoeAcessandoAtributos.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/definindoeAcessandoAtributos.py
@@ -12,3 +12,13 @@ jogador2 = Jogador("Carlos", "Goleiro", 1)
 print(f"{jogador1.nome} é um {jogador1.posicao} com a camisa de número {jogador1.numero_camisa}") # Roberto é um Atacante com a camisa de número 9
 
 print(f"{jogador2.nome} é um {jogador2.posicao} com a camisa de número {jogador2.numero_camisa}") # Carlos é um Goleiro com a camisa de número 1
+
+jogador1.gols += 1
+jogador1.gols += 1
+jogador1.gols += 1
+
+print(f"{jogador1.nome} marcou {jogador1.gols} gol(s)") # Roberto marcou 3 gol(s)
+
+jogador2.gols += 2
+
+print(f"{jogador2.nome} marcou {jogador2.gols} gol(s)") # Carlos marcou 2 gol(s)
