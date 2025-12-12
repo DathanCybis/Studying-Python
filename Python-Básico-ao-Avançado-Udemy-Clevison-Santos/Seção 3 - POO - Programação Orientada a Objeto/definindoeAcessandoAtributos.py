10d29d200c45b19e6526b330bf7b221fca7427a0
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-11 22:09:27 -0300

    Update definindoeAcessandoAtributos.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/definindoeAcessandoAtributos.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/definindoeAcessandoAtributos.py
@@ -5,3 +5,10 @@ class Jogador:
         self.numero_camisa = numero_camisa
         self.gols = gols
 
+jogador1 = Jogador("Roberto", "Atacante", 9)
+
+jogador2 = Jogador("Carlos", "Goleiro", 1)
+
+print(f"{jogador1.nome} é um {jogador1.posicao} com a camisa de número {jogador1.numero_camisa}") # Roberto é um Atacante com a camisa de número 9
+
+print(f"{jogador2.nome} é um {jogador2.posicao} com a camisa de número {jogador2.numero_camisa}") # Carlos é um Goleiro com a camisa de número 1
