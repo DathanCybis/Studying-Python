214c1ae61bd518e60db8e1b99a5d57dfd1045e38
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-13 22:38:28 -0300

    Update métodosPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/métodosPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/métodosPOO.py
@@ -2,4 +2,8 @@ class Termostato:
     def __init__(self, temperatura_atual=20):
         self.temperatura_atual = temperatura_atual
 
+    def aumentar_temperatura(self, valor):
+        self.temperatura_atual += valor
+        print(f"Temperatura aumentada em: {valor}, Nova temperatura: {self.temperatura_atual}")
+
     
