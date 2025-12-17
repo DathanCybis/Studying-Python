b76d7d82d1a55fbb855678b0b7552cd1f3afe842
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-17 10:21:46 -0300

    Create ExManipuladorDeLista.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExManipuladorDeLista.py
@@ -0,0 +1,15 @@
+lista = []
+class ManipuladorDeLista:
+    def __init__(self, elemento):
+        self.elemento = elemento
+
+
+    def adicionar_elemento(elemento):
+        num = input("Digite um número inteiro para adicionar: ")
+
+        if num:
+            lista.append(num)
+        else:
+            print("Digite um número válido!")
+
+
