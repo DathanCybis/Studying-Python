63d89d0054c51b40d33bc8df47e0763cf07bb412
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-17 11:27:57 -0300

    Update ExManipuladorDeLista.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExManipuladorDeLista.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExManipuladorDeLista.py
@@ -1,54 +1,63 @@
-lista = []
 class ManipuladorDeLista:
-    def __init__(self, elemento):
-        self.elemento = elemento
+    def __init__(self):
+        self.lista = []
 
 
-    def adicionar_elemento():
+    def adicionar_elemento(self):
         try:
             num = int(input("Digite um número inteiro para adicionar: "))
         except:
             print("Por favor, digite um número inteiro!")
             return
 
-        lista.append(num)
+        self.lista.append(num)
         print(f"Elemento '{num}' adicionado com sucesso!")
 
 
-    def remover_elemento():
+    def remover_elemento(self):
         try:
             num = int(input("Digite o número inteiro que deseja remover: "))
         except:
             print("Por favor, digite um número inteiro!")
             return
 
-        if num in lista:
-            lista.remove(num)
+        if num in self.lista:
+            self.lista.remove(num)
             print(f"Elemento '{num}' removido com sucesso!")
         else:
             print("Por favor, digite um número inteiro válido!")
 
 
-    def encontrar_maior():
-        maior = max(lista)
-        print(f"O maior elemento da lista é o: '{maior}'")
+    def encontrar_maior(self):
+        if self.lista:
+            maior = max(self.lista)
+            print(f"O maior elemento da lista é o: '{maior}'")
+        else:
+            print("A lista está vazia.")
 
 
-    def encontrar_menor():
-        menor = min(lista)
-        print(f"O menor elemento da lista é o: '{menor}'")
+    def encontrar_menor(self):
+        if self.lista:
+            menor = min(self.lista)
+            print(f"O menor elemento da lista é o: '{menor}'")
+        else:
+            print("A lista está vazia.")
 
 
-    def calcular_media():
-        media = sum((lista)) / len(lista)
-        print(f"A média dos elementos da lista é: '{media}'")
+    def calcular_media(self):
+        if self.lista:
+            media = sum((self.lista)) / len(self.lista)
+            print(f"A média dos elementos da lista é: '{media}'")
+        else:
+            print("A lista está vazia.")
 
 
-    def mostrar_lista():
-        print(f"A lista atual é: {lista}")
+    def mostrar_lista(self):
+        print(f"A lista atual é: {self.lista}")
 
 
 def menu():
+    manipulador = ManipuladorDeLista()
     while True:
         print("\nEscolha uma opção:")
         print("1. Adicionar elemento")
@@ -62,21 +71,23 @@ def menu():
         opc = input("\nDigite a opção desejada: ")
 
         if opc == "1":
-            ManipuladorDeLista.adicionar_elemento()
+            manipulador.adicionar_elemento()
         elif opc == "2":
-            ManipuladorDeLista.remover_elemento()
+            manipulador.remover_elemento()
         elif opc == "3":
-            ManipuladorDeLista.encontrar_maior()
+            manipulador.encontrar_maior()
         elif opc == "4":
-            ManipuladorDeLista.encontrar_menor()
+            manipulador.encontrar_menor()
         elif opc == "5":
-            ManipuladorDeLista.calcular_media()
+            manipulador.calcular_media()
         elif opc == "6":
-            ManipuladorDeLista.mostrar_lista()
+            manipulador.mostrar_lista()
         elif opc == "7":
             print("Fim do programa...\n")
             break
         else:
             print("Por favor, digite uma opção válida!")
 
-menu()
+
+if __name__ == "__main__":
+    menu()
