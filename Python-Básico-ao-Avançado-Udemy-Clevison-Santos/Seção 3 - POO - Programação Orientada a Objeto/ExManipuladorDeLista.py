f3617f91e02a804640d18a901ebab9728f455568
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-17 10:35:58 -0300

    Update ExManipuladorDeLista.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExManipuladorDeLista.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExManipuladorDeLista.py
@@ -1,15 +1,38 @@
-lista = []
+lista = [2, 3]
 class ManipuladorDeLista:
     def __init__(self, elemento):
         self.elemento = elemento
 
 
-    def adicionar_elemento(elemento):
+    def adicionar_elemento():
         num = input("Digite um número inteiro para adicionar: ")
 
-        if num:
-            lista.append(num)
+        if num == int:
+            lista.append(int(num))
         else:
-            print("Digite um número válido!")
+            print("Por favor, digite um número inteiro!")
 
 
+    def remover_elemento():
+        try:
+            num = int(input("Digite o número inteiro que deseja remover: "))
+        except:
+            print("Por favor, digite um número inteiro!")
+            return
+
+        if num in lista:
+            lista.remove(num)
+            print(f"Elemento '{num}' removido com sucesso!")
+        else:
+            print("Por favor, digite um número inteiro válido!")
+
+
+    def encontrar_maior():
+        maior = max(lista)
+        print(f"O maior elemento da lista é: {maior}")
+
+
+numero = ManipuladorDeLista.encontrar_maior()
+
+print(lista)
+print(numero)
