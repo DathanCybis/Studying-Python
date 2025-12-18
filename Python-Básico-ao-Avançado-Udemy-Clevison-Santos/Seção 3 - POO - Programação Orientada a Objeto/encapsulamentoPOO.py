0b6a330ac92b0f7c35a32f81a094cff08f06fe7b
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-18 10:47:38 -0300

    Update encapsulamentoPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/encapsulamentoPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/encapsulamentoPOO.py
@@ -20,3 +20,29 @@ class Pessoa:
     def __aumentar_saldo(self, quant):
         self.__saldo += quant
 
+
+    def depositar(self, quant):
+        self.__aumentar_saldo(quant)
+
+        return self.__saldo
+    
+
+p = Pessoa("Alice", 30)
+
+print(p.nome) # Alice
+
+print(p._idade) # 30 #Não recomendado
+
+print(p._Pessoa__saldo) # 0 #Não recomendado
+
+
+# --- Métodos Públicos --- 
+print("\nMétodos Públicos:")
+print(p.mostrar_nome()) # Alice
+print(p.mostrar_idade()) # 30
+print(p.depositar(1500)) # 1500
+print(p.depositar(1000)) # 2500
+print(p.depositar(3500)) # 6000
+
+# --- Métodos Privados --- 
+print("\nMétodos Privados:")
