88a30c0a8a911ef7a335af677b94e552af17b3e9
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-18 10:32:56 -0300

    Create encapsulamentoPOO.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/encapsulamentoPOO.py
@@ -0,0 +1,18 @@
+class Pessoa:
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self._idade = idade
+        self.__saldo = 0
+
+
+    def mostrar_nome(self):
+        return self.nome
+
+
+    def mostrar_idade(self):
+        return self._idade
+    
+
+    def _aumentar_idade(self):
+        self._idade += 1
+
