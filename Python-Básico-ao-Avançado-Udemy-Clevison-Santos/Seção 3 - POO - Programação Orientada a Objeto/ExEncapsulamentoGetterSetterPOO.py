807933fdb3826a48a34ed1ee997b0a36a6f7c860
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-19 11:25:56 -0300

    Update ExEncapsulamentoGetterSetterPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExEncapsulamentoGetterSetterPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExEncapsulamentoGetterSetterPOO.py
@@ -5,15 +5,24 @@ class Pet:
         self._peso = 0.0
 
 
+    def get_nome(self):
+        return self._nome
+
+
+    def get_idade(self):
+        return self._idade
+
+
+    def get_peso(self):
+        return self._peso
+        
+
     def set_nome(self, nome):
-        try:
-            str(nome)
-            if nome.isalpha():
-                self._nome = nome
-            else:
-                print("O nome deve ser uma string e não vazio")
-        except:
-            print("O nome deve ser uma string e não vazio!")
+        if isinstance(nome, str) and nome != "":
+            self._nome = nome
+        else:
+            print("O nome deve ser uma string e não vazio")
+
         
     
     def set_idade(self, idade):
@@ -25,20 +34,34 @@ class Pet:
 
     def set_peso(self, peso):
         peso = float(peso)
-        if peso >= 0:
+        if peso > 0:
             self._peso = peso
         else:
-            print("O peso deve ser flutuante e maior ou igual a 0")
+            print("O peso deve ser flutuante e maior que 0")
 
 
     def exibir_infos(self):
-        print(f"O nome do pet é {self._nome}")
-        print(f"A idade do pet é {self._idade}")
-        print(f"O peso do pet é {self._peso}")
+        print(f"Nome: {self._nome}")
+        print(f"Idade: {self._idade}")
+        print(f"Peso: {self._peso} kg")
 
     
 meu_pet = Pet()
-meu_pet.set_nome("")
+meu_pet.set_nome("Bob")
 meu_pet.set_idade(2)
 meu_pet.set_peso(4)
+meu_pet.exibir_infos()
+
+print("\n -------------- \n")
+
+meu_pet.set_nome("Lua")
+meu_pet.set_idade(4)
+meu_pet.set_peso(8.3)
+meu_pet.exibir_infos()
+
+print("\n -------------- \n")
+
+meu_pet.set_nome("Jaspion")
+meu_pet.set_idade(8)
+meu_pet.set_peso(9.3)
 meu_pet.exibir_infos()
