961178b8d3a0994d0e725fb810f9f5cf696ede6e
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-19 10:50:05 -0300

    Update ExEncapsulamentoGetterSetterPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExEncapsulamentoGetterSetterPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExEncapsulamentoGetterSetterPOO.py
@@ -5,3 +5,27 @@ class Pet:
         self._peso = peso
 
 
+    def get_nome(self):
+        return self._nome
+    
+
+    def get_idade(self):
+        return self._idade
+
+
+    def get_peso(self):
+        return self._peso
+    
+
+    def set_nome(self, nome):
+        print(f"O nome do pet é {nome}")
+    
+
+    def set_idade(self, idade):
+        print(f"A idade do pet é {idade}")
+
+
+    def set_peso(self, peso):
+        print(f"O peso do pet é {peso}")
+
+    
