39385a220595542d8aff30e40bb823c7aab4e6d9
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-19 10:58:14 -0300

    Update ExEncapsulamentoGetterSetterPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExEncapsulamentoGetterSetterPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExEncapsulamentoGetterSetterPOO.py
@@ -1,5 +1,5 @@
 class Pet:
-    def __init__(self, nome, idade, peso):
+    def __init__(self, nome=None, idade=None, peso=None):
         self._nome = nome
         self._idade = idade
         self._peso = peso
@@ -18,14 +18,29 @@ class Pet:
     
 
     def set_nome(self, nome):
-        print(f"O nome do pet é {nome}")
+        try:
+            if nome == str(nome):
+                self._nome = nome
+        except:
+            print("O nome deve ser uma string e não vazio")
+        
     
-
     def set_idade(self, idade):
-        print(f"A idade do pet é {idade}")
+        self._idade = idade
 
 
     def set_peso(self, peso):
-        print(f"O peso do pet é {peso}")
+        self._peso = peso
+
+
+    def exibir_infos(self):
+        print(f"O nome do pet é {self._nome}")
+        print(f"A idade do pet é {self._idade}")
+        print(f"O peso do pet é {self._peso}")
 
     
+meu_pet = Pet()
+meu_pet.set_nome("")
+meu_pet.set_idade(2)
+meu_pet.set_peso(4)
+meu_pet.exibir_infos()
