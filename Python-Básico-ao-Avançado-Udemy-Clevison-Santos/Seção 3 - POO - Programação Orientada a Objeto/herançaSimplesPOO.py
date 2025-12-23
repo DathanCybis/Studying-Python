a5dd3df76be49987f0c122670cb9d513565abbad
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-23 10:38:43 -0300

    Update herançaSimplesPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/herançaSimplesPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/herançaSimplesPOO.py
@@ -5,28 +5,38 @@ class Pessoa:
 
 
     def exibir_info(self):
-        print(f"Nome: {self.nome}\nIdade: {self.idade}")
+        print(f"Nome: {self.nome}, Idade: {self.idade}")
 
 
 class Estudante(Pessoa):
     def __init__(self, nome, idade, matricula):
 
-        Pessoa.__init__(nome, idade)
+        Pessoa.__init__(self, nome, idade)
         self.matricula = matricula
 
     
     def estudar(self):
-        print(f"{self.nome} está estudando)")
+        print(f"{self.nome} está estudando.")
 
 
 class Professor(Pessoa):
     def __init__(self, nome, idade, disciplina):
 
-        Pessoa.__init__(nome, idade)
+        Pessoa.__init__(self, nome, idade)
         self.disciplina = disciplina
 
 
     def ensinando(self):
         print(f"{self.nome} está ensinando {self.disciplina}")
 
+
+pessoa = Pessoa("Maria", 40)
+
+estudante = Estudante("João", 18, "12345")
+
+professor = Professor("Carlos", 50, "Matemática")
+
+pessoa.exibir_info() # Nome: Maria, Idade: 40
+estudante.exibir_info() # Nome: João, Idade: 18
+estudante.estudar() # João está estudando.
 
