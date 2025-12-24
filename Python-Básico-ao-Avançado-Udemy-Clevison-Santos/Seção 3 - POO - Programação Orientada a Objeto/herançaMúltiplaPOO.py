5c99569960eb594f1ef87a11262274e92420d259
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-24 17:41:09 -0300

    Update herançaMúltiplaPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/herançaMúltiplaPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/herançaMúltiplaPOO.py
@@ -7,3 +7,31 @@ class Mamifero:
         print("Amamentando...")
 
 
+class Ave:
+    def __init__(self):
+        print("Sou uma ave...")
+
+
+    def voar(self):
+        print("Voando...")
+
+
+class Morcego(Mamifero, Ave):
+    def __init__(self):
+        Mamifero.__init__(self)
+        Ave.__init__(self)
+
+        print("Sou um morcego...")
+
+
+    def emitir_som(self):
+        print("Emitindo som de ecolocalização...")
+
+
+morcego = Morcego() # Sou um mamifero... \n # Sou uma ave... \n # Sou um morcego...
+
+morcego.amamentar() # Amamentando...
+
+morcego.voar() # Voando...
+
+morcego.emitir_som() # Emitindo som de ecolocalização...
