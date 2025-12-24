4388db2553fe046194ce77af03e33d93ebc98e76
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-24 17:51:56 -0300

    Update ExherançaMúltiplaPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExherançaMúltiplaPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExherançaMúltiplaPOO.py
@@ -1,28 +1,15 @@
 class Musico:
-    def __init__(self):
-        pass
-
-
     def tocar_instrumento(self):
         print("Tocando instrumento musical")
 
 
 class Atleta:
-    def __init__(self):
-        pass
-
-
     def correr(self):
         print("Correndo na pista")
 
 
 
 class MusicoAtleta(Musico, Atleta):
-    def __init__(self):
-        Musico.__init__(self)
-        Atleta.__init__(self)
-
-    
     def exibir_habilidades(self):
         print("Tocando instrumento e correndo")
 
