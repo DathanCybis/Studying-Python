1e0473a9f2d020597ab2e2e5c2637d3799e156e3
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-25 21:58:12 -0300

    Update ExFunçãoSuper.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExFunçãoSuper.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExFunçãoSuper.py
@@ -1,18 +1,28 @@
 class Veiculo:
+    def __init__(self, marca, modelo):
+        self.marca = marca
+        self.modelo = modelo
+
+
     def exibir_info(self):
-        print(f"Marca: Ferrari")
+        print(f"Marca: {self.marca}, Modelo: {self.modelo}")
 
 
 class Carro(Veiculo):
+    def __init__(self, marca, modelo, cor):
+        super().__init__(marca, modelo)
+        self.cor = cor
+
+
     def exibir_info(self):
         super().exibir_info()
-        print(f"Cor: Vermelho")
+        print(f"Cor: {self.cor}")
 
 
-veiculo = Veiculo()
+veiculo = Veiculo("Honda", "Civic")
 
-veiculo.exibir_info() # Marca: Ferrari
+veiculo.exibir_info() # Marca: Honda, Modelo: Civic
 
-carro = Carro()
+carro = Carro("Toyota", "Corolla", "Chumbo")
 
-carro.exibir_info() # Marca: Ferrari \n Cor: Vermelho
+carro.exibir_info() # Marca: Toyota, Modelo: Corolla \n Cor: Chumbo
