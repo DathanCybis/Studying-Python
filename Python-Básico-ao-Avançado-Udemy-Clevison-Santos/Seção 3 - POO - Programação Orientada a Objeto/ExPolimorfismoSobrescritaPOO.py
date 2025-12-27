0dbd8e32579ae661150cc944057251fc58253f1e
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-27 12:23:47 -0300

    Create ExPolimorfismoSobrescritaPOO.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExPolimorfismoSobrescritaPOO.py
@@ -0,0 +1,19 @@
+class Veiculo():
+    def mover(self):
+        print("O veículo está se movendo")
+
+
+class Carro():
+    def mover(self):
+        print("O carro está se movendo na estrada")
+
+
+class Barco():
+    def mover(self):
+        print("O barco está se movendo na água")
+
+
+class Aviao():
+    def mover(self):
+        print("O avião está se movendo no céu")
+
