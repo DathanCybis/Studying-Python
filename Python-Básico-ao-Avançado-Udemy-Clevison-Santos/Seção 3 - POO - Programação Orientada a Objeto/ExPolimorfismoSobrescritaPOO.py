0fe70c607a3c8c9487f6857cac21a12e72f27227
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-27 12:23:52 -0300

    Update ExPolimorfismoSobrescritaPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExPolimorfismoSobrescritaPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExPolimorfismoSobrescritaPOO.py
@@ -17,3 +17,20 @@ class Aviao():
     def mover(self):
         print("O avião está se movendo no céu")
 
+
+veiculo = Veiculo()
+
+carro = Carro()
+
+barco = Barco()
+
+aviao = Aviao()
+
+
+veiculo.mover() # O veículo está se movendo
+
+carro.mover() # O carro está se movendo na estrada
+
+barco.mover() # O barco está se movendo na água
+
+aviao.mover() # O avião está se movendo no céu
