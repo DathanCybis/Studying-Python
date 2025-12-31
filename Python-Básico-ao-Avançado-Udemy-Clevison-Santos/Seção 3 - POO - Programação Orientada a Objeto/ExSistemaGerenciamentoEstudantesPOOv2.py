f8a4627c827f9ddbf7f9b9ddc4ea0b9aab229417
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2025-12-31 17:30:12 -0300

    Update ExSistemaGerenciamentoEstudantesPOOv2.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExSistemaGerenciamentoEstudantesPOOv2.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExSistemaGerenciamentoEstudantesPOOv2.py
@@ -30,4 +30,44 @@ class Estudante:
         self.nota = nota
     
 
+def menu():
+    estudantes = []
+
+    while True:
+        print("\n --- Menu --- ")
+        print("1. Adicionar um novo estudante")
+        print("2. Atualizar a nota de um estudante existente")
+        print("3. Visualizar informações de um estudante")
+        print("4. Listar todos os estudantes")
+        print("5. Sair")
+
+        opc = input("\nDigite a opção desejada: ")
+
+        if opc == "1":
+            nome = input("Nome: ")
+            idade = int(input("Idade: "))
+            nota = float(input("Nota: "))
+
+            novo_estudante = Estudante(nome, idade, nota)
+
+            estudantes.append(novo_estudante)
+
+            print(f"Estudante {nome} adicionado com sucesso!")
+
+
+        elif opc == "2":
+            pass
+
     
+        elif opc == "3":
+            estudante.visualizar_estudante()
+        elif opc == "4":
+            estudante.listar_estudantes()
+        elif opc == "5":
+            print("Saindo...")
+            break
+        else:
+            print("Tente novamente, opção inválida!")
+
+
+menu()
