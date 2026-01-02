ee2d38b4d502cc8131ffee4fad3a32be07ff0361
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-01 23:14:05 -0300

    Update ExAgendaContatosPOO.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExAgendaContatosPOO.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExAgendaContatosPOO.py
@@ -5,10 +5,6 @@ class Contato:
         self.email = email
 
 
-
-
-
-
 class Agenda:
     def adicionar_contatos(self):
         pass
@@ -21,3 +17,32 @@ class Agenda:
 
     def buscar_contatos(self):
         pass
+
+
+def menu():
+    agenda = Agenda()
+    while True:
+        print("1. Adicionar contatos")
+        print("2. Remover contatos")
+        print("3. Listar contatos")
+        print("4. Buscar contato")
+        print("5. Sair")
+
+        opc = input("Digite a opção desejada: ")
+
+        if opc == "1":
+            agenda.adicionar_contatos()
+        elif opc == "2":
+            agenda.remover_contatos()
+        elif opc == "3":
+            agenda.listar_contatos()
+        elif opc == "4":
+            agenda.buscar_contatos()
+        elif opc == "5":
+            print("Saindo...")
+            break
+        else:
+            print("Tente novamente, por favor digite uma opção válida!")
+
+
+menu()
