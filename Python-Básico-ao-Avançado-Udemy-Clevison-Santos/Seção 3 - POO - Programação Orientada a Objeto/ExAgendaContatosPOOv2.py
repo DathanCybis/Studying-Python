d2b6b92288d4b8f6e88848787d49f329bb743dfe
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-02 21:39:06 -0300

    Update ExAgendaContatosPOOv2.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExAgendaContatosPOOv2.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExAgendaContatosPOOv2.py
@@ -37,3 +37,63 @@ class Agenda():
         return None
 
 
+def menu():
+    agenda = Agenda()
+    while True:
+        print("\n1. Adicionar contatos")
+        print("2. Remover contatos")
+        print("3. Listar contatos")
+        print("4. Buscar contato")
+        print("5. Sair\n")
+
+        opc = input("Digite a opção desejada: ")
+
+        print()
+
+        if opc == "1":
+            nome = input("Nome: ")
+            telefone = input("Telefone: ")
+            email = input("Email: ")
+
+            novo_contato = Contato(nome, telefone, email)
+            agenda.adicionar_contatos(novo_contato)
+
+            print("Contato adicionado com sucesso!")
+
+
+        elif opc == "2":
+            nome = input("Nome do contato que deseja excluir: ")
+
+            if agenda.remover_contatos(nome):
+                print("Contato removido com sucesso.")
+            else:
+                print("Contato não encontrado.")
+
+
+        elif opc == "3":
+            print("Listando todos os contatos...")
+
+            for contato in agenda.listar_contatos():
+                    print(f"Nome: {contato.nome}, Telefone: {contato.telefone}, Email: {contato.email}")
+
+
+        elif opc == "4":
+            nome = input("Nome do contato que deseja encontrar: ")
+
+            contato = agenda.buscar_contatos(nome)
+
+            if contato:
+                print(f"Nome: {contato.nome}, Telefone: {contato.telefone}, Email: {contato.email}")
+            else:
+                print("Contato não encontrado.")
+
+
+        elif opc == "5":
+            print("Saindo...")
+            break
+        else:
+            print("Tente novamente, por favor digite uma opção válida!")
+
+
+if __name__ == "__main__":
+    menu()
