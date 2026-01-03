44854374f2490bf4eb09549de1144e34fa7cb68a
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-02 21:38:59 -0300

    Update ExAgendaContatosPOOv2.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExAgendaContatosPOOv2.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 3 - POO - Programação Orientada a Objeto/ExAgendaContatosPOOv2.py
@@ -5,3 +5,35 @@ class Contato:
         self.email = email
 
 
+class Agenda():
+    def __init__(self):
+        self.contatos = []
+
+
+    def adicionar_contatos(self, contato):
+        self.contatos.append(contato)
+
+
+    def remover_contatos(self, nome):
+        for contato in self.contatos:
+
+            if contato.nome == nome:
+                self.contatos.remove(contato)
+                return True
+
+        return False
+
+
+    def listar_contatos(self):
+        return self.contatos
+        
+
+    def buscar_contatos(self, nome):
+        for contato in self.contatos:
+
+            if contato.nome == nome:
+                return contato
+            
+        return None
+
+
