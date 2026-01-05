e2953dc343ca2c63fdbf61e56ee6ae0269b507ae
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-05 13:08:12 -0300

    Update ScriptParaAbrirFecharNotePad.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 6 - Robô 1 - Controlando o computador/ScriptParaAbrirFecharNotePad.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 6 - Robô 1 - Controlando o computador/ScriptParaAbrirFecharNotePad.py
@@ -2,9 +2,16 @@ import pyautogui as pag
 
 pag.hotkey('win', 'r')
 
-pag.sleep(0.5)
+pag.sleep(1)
 
 pag.typewrite('notepad')
 
-pag.sleep(0.5)
+pag.sleep(2)
 
+pag.press('enter')
+
+pag.sleep(3)
+
+pag.typewrite('O notepad foi aberto')
+
+pag.sleep(2)
