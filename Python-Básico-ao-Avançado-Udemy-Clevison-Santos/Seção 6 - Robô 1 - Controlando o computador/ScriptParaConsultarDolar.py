86e8b97b00f554ec4b8df00c6a61d8daa7971e9e
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-04 21:07:21 -0300

    Update ScriptParaConsultarDolar.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 6 - Robô 1 - Controlando o computador/ScriptParaConsultarDolar.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 6 - Robô 1 - Controlando o computador/ScriptParaConsultarDolar.py
@@ -1,5 +1,17 @@
 import pyautogui as pag
 
-pag.sleep(2)
+#pag.sleep(2)
 
 pag.click(x=735, y=1053)
+
+pag.typewrite('Edge')
+
+pag.sleep(1)
+
+#print(pag.position())
+
+pag.click(x=723, y=489)
+
+pag.sleep(1)
+
+pag.typewrite('Dolar hoje')
