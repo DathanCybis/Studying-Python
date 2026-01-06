2e11af96e2e98b21496499fd7faf662d42706a34
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-06 15:28:24 -0300

    Update ScriptMenuOpções.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 6 - Robô 1 - Controlando o computador/ScriptMenuOpções.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 6 - Robô 1 - Controlando o computador/ScriptMenuOpções.py
@@ -5,6 +5,28 @@ opcao = pyautogui.confirm('Clique no botão desejado', buttons = ['Word', 'Googl
 
 if opcao == "Word":
     print("Escolheu Word")
+
+    pag.hotkey('win', 'r')
+
+    pag.sleep(0.5)
+
+    pag.typewrite('winword')
+
+    pag.sleep(0.5)
+
+    pag.press('enter')
+
+    pag.sleep(1)
+
+    pag.click(x=268, y=219)
+
+    pag.sleep(0.5)
+
+    pag.typewrite("Escolheu abrir o word")
+
+    #print(pag.position())
+
+
 elif opcao == "Google":
     print("Escolheu Google")
 elif opcao == "Notepad":
