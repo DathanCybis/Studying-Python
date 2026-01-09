0903aa0a50053c549552e8907037015c4f666b41
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-08 21:47:57 -0300

    Update GoogleConsultandoDolar.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 7 - Robô 2 - Extraindo Dólar e Euro da Internet e Salvando no Excel/GoogleConsultandoDolar.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 7 - Robô 2 - Extraindo Dólar e Euro da Internet e Salvando no Excel/GoogleConsultandoDolar.py
@@ -6,3 +6,10 @@ from selenium.webdriver.common.by import By
 navegador = opcoes.Chrome()
 
 navegador.get("https://www.google.com/")
+
+pag.sleep(1)
+
+navegador.find_element(By.NAME, "q").send_keys("Dolar hoje")
+
+pag.sleep(1)
+
