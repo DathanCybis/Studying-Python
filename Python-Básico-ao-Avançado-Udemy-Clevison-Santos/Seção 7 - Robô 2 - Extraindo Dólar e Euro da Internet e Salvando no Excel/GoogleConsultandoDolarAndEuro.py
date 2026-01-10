f38d2f8b7d36d42cb14cf06f30063502f980d794
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-09 23:08:59 -0300

    Create GoogleConsultandoDolarAndEuro.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 7 - Robô 2 - Extraindo Dólar e Euro da Internet e Salvando no Excel/GoogleConsultandoDolarAndEuro.py
@@ -0,0 +1,30 @@
+from selenium import webdriver as opcoes
+from selenium.webdriver.common.keys import Keys
+import pyautogui as pag
+from selenium.webdriver.common.by import By
+
+navegador = opcoes.Chrome()
+
+navegador.get("https://www.google.com/")
+
+pag.sleep(1)
+
+navegador.find_element(By.NAME, "q").send_keys("Dolar hoje")
+
+pag.sleep(1)
+
+navegador.find_element(By.NAME, "q").send_keys(Keys.RETURN)
+
+pag.sleep(1)
+
+valorDolar = navegador.find_elements(By.XPATH, '//*[@id="knowledge-currency__updatable-data-column"]/div[1]/div[2]/span[1]')[0].text
+
+pag.sleep(1)
+
+print(valorDolar)
+
+pag.sleep(1)
+
+navegador.find_element(By.NAME, "q").send_keys("")
+
+pag.sleep(1)
