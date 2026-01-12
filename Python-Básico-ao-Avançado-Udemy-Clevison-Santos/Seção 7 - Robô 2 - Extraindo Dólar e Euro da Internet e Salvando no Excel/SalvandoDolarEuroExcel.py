df0e4828b9bc1980e86970ce97625967a8196f96
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-12 19:44:41 -0300

    Update SalvandoDolarEuroExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 7 - Robô 2 - Extraindo Dólar e Euro da Internet e Salvando no Excel/SalvandoDolarEuroExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 7 - Robô 2 - Extraindo Dólar e Euro da Internet e Salvando no Excel/SalvandoDolarEuroExcel.py
@@ -54,3 +54,28 @@ def obter_cotacao_euro():
 # Chamada da função
 obter_cotacao_dolar()
 obter_cotacao_euro()
+
+import os
+import xlsxwriter
+
+# Caminho onde o arquivo será salvo (pasta atual)
+caminho = os.path.join(os.getcwd(), "dados.xlsx")
+
+# Cria o arquivo Excel
+workbook = xlsxwriter.Workbook(caminho)
+
+# Cria uma planilha
+worksheet = workbook.add_worksheet("Planilha1")
+
+# Escrevendo os dados
+worksheet.write("A1", "Nome")
+worksheet.write("B1", "João")
+
+worksheet.write("A2", "Idade")
+worksheet.write("B2", 25)
+
+# Fecha o arquivo (muito importante)
+workbook.close()
+
+print("Planilha criada com sucesso!")
+
