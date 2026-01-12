6932656db52b1d419dc179098a178950b7c7f9a6
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-12 19:36:38 -0300

    Update xlsxwriterCriandoArquivoExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 7 - Robô 2 - Extraindo Dólar e Euro da Internet e Salvando no Excel/xlsxwriterCriandoArquivoExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 7 - Robô 2 - Extraindo Dólar e Euro da Internet e Salvando no Excel/xlsxwriterCriandoArquivoExcel.py
@@ -5,4 +5,10 @@ caminhoArquivo = "C\\...\\..."
 
 planilha = xlsxwriter.workbook(caminhoArquivo)
 
+planilha1 = planilha.add_worksheet()
 
+planilha1.write("A1", "Nome")
+
+planilha.close()
+
+os.startfile(caminhoArquivo)
