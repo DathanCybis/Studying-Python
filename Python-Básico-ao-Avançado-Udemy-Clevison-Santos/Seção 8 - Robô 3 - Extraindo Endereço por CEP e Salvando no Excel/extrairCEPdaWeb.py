d0ac93f1cf8639f6c6b99702390ce5dc337cd843
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-14 09:33:11 -0300

    Update extrairCEPdaWeb.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 8 - Robô 3 - Extraindo Endereço por CEP e Salvando no Excel/extrairCEPdaWeb.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 8 - Robô 3 - Extraindo Endereço por CEP e Salvando no Excel/extrairCEPdaWeb.py
@@ -2,5 +2,25 @@ import http.client
 
 import json
 
+def obter_cep(cep):
+    conexao = http.client.HTTPSConnection("viacep.com.br")
+
+    conexao.request("GET", f"/ws/{cep}/json/")
+
+    resposta = conexao.getresponse()
+
+    dados = resposta.read()
+
+    endereco = json.loads(dados.decode("utf-8"))
+
+    if "erro" not in endereco:
+        return endereco
+    else:
+        return "CEP não encontrado."
+
+
 cep_ex = "01001000"
 
+endereco = obter_cep(cep_ex)
+
+print(endereco)
