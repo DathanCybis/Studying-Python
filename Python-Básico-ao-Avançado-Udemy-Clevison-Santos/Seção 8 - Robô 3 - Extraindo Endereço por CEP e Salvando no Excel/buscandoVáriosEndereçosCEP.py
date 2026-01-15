91ff54c1230a2eccfde0f596ff2509b4b3609e1b
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-15 10:52:33 -0300

    Create buscandoVáriosEndereçosCEP.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 8 - Robô 3 - Extraindo Endereço por CEP e Salvando no Excel/buscandoVáriosEndereçosCEP.py
@@ -0,0 +1,37 @@
+import http.client
+import json
+import pandas as pd
+
+def obter_cep(cep):
+    conexao = http.client.HTTPSConnection("viacep.com.br")
+
+    conexao.request("GET", f"/ws/{cep}/json/")
+
+    resposta = conexao.getresponse()
+
+    dados = resposta.read()
+
+    endereco = json.loads(dados.decode("utf-8"))
+
+    return endereco
+
+
+def salvar_endereco(endereco, nome_arquivo="endereco.xlsx"):
+    if "erro" not in endereco:
+        df = pd.DataFrame([endereco])
+
+        df.to_excel(nome_arquivo, index=False)
+
+        print(f"Dados salvos com sucesso no arquivo {nome_arquivo}")
+
+    else:
+        print("Não foi possível salvar os dados: CEP não encontrado.")
+
+
+cep_ex = "01001000"
+
+endereco = obter_cep(cep_ex)
+
+salvar_endereco(endereco)
+
+print(endereco)
