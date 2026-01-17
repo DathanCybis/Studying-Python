74510e9f58ed4af71dfeb25f018c35325affad2d
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-17 19:54:11 -0300

    Update CorArquivoExcelEmPython.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/CorArquivoExcelEmPython.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/CorArquivoExcelEmPython.py
@@ -1,18 +1,22 @@
 import xlsxwriter as xls
 import os
 
-nomeCaminho = '#caminho C:\\...\\...\\....xlsx'
+nomeCaminho = 'C:\\...\\...\\pintafundoefonte.xlsx'
 
 workbook = xls.Workbook(nomeCaminho)
-sheetPadrao = workbook.add_worksheet()
+sheetDados = workbook.add_worksheet("Dados")
 
+corFundo = workbook.add_format({"fg_color":'yellow'})
 
-sheetPadrao.write("A1", "Nome")
-sheetPadrao.write("B1", "Idade")
-sheetPadrao.write("A2", "Amanda")
-sheetPadrao.write("B2", 21)
-sheetPadrao.write("A3", "Allan")
-sheetPadrao.write("B3", 28)
+corFonte = workbook.add_format()
+corFonte.set_font_color('blue')
+
+sheetDados.write("A1", "Nome", corFundo)
+sheetDados.write("B1", "Idade", corFundo)
+sheetDados.write("A2", "Amanda", corFonte)
+sheetDados.write("B2", 21, corFonte)
+sheetDados.write("A3", "Allan", corFonte)
+sheetDados.write("B3", 28, corFonte)
 
 workbook.close()
 
