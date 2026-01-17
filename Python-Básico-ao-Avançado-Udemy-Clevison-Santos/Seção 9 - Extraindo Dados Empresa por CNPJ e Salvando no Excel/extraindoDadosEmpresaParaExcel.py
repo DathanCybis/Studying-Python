4db7c34ba7736d3761126d1bd0cff6c7021f82fc
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-16 23:43:01 -0300

    Update extraindoDadosEmpresaParaExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 9 - Extraindo Dados Empresa por CNPJ e Salvando no Excel/extraindoDadosEmpresaParaExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 9 - Extraindo Dados Empresa por CNPJ e Salvando no Excel/extraindoDadosEmpresaParaExcel.py
@@ -6,5 +6,15 @@ def obter_dados(cnpj):
 
     conexao.request("GET", f"/v1/cnpj/cnpj")
 
+    resposta = conexao.getresponse()
+
+    dados = resposta.read
+
+    empresa = json.loads(dados.decode("utf-8"))
+
+    conexao.close()
+
 
 cnpj_ex = "33157312000162"
+
+dados_empresa = obter_dados(cnpj_ex)
