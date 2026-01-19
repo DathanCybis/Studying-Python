acf0ed876f67013e8212c3ad3665c22cf1c328c5
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-19 11:34:06 -0300

    Update FormulasExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/FormulasExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/FormulasExcel.py
@@ -19,21 +19,19 @@ sheetDados.write("A2", 10)
 sheetDados.write("A3", 6)
 sheetDados.write("A4", 8)
 sheetDados.write("A5", 2)
-
+sheetDados.write("A8", "Ana")
 
 sheetDados.write("B2", 9)
 sheetDados.write("B3", 5)
 sheetDados.write("B4", 7)
 sheetDados.write("B5", 3)
-
+sheetDados.write("B8", "Paula")
 
 sheetDados.write("C2", "=A2+B2")
 sheetDados.write("C3", "=A3-B3")
 sheetDados.write("C4", "=A4*B4")
 sheetDados.write("C5", "=A5/B5")
-
-
-
+sheetDados.write("C8", '=CONCATENATE(A8," ",B8)')
 
 workbook.close()
 
