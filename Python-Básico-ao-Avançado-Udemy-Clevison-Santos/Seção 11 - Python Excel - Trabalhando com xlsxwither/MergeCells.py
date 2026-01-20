fe29443384178882b05fc4716848e552c13b7605
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-20 11:30:02 -0300

    Update MergeCells.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/MergeCells.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/MergeCells.py
@@ -1,20 +1,21 @@
 import xlsxwriter as xls
 import os
 
-nomeCaminho = 'C:\\...\\...\\Formulas.xlsx'
+nomeCaminho = 'C:\\...\\...\\MergeCells.xlsx'
 
 workbook = xls.Workbook(nomeCaminho)
 sheetDados = workbook.add_worksheet("Dados")
 
 add_merge = workbook.add_format({
-    'bold:' True,
+    'bold': True,
     'border': 6,
     'valign':'vcemter',
     'size':30,
     'fg_color':'blue',
     'font_color':'white'
-
 })
+
+sheetDados.merge_range('B3:I5', 'Merge Células', add_merge)
 
 workbook.close()
 
