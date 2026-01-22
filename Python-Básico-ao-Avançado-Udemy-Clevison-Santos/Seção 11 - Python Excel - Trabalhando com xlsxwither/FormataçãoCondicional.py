6e3f7117a001fa71e58b82a4d67e943b22e0507b
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-22 19:10:58 -0300

    Update FormataçãoCondicional.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/FormataçãoCondicional.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/FormataçãoCondicional.py
@@ -9,11 +9,11 @@ sheetDados = workbook.add_worksheet("Dados")
 
 formatoMaior = workbook.add_format({
     'bg_color': 'green',
-    'fort_color': 'white'
+    'font_color': 'white'
 })
 formatoMenor = workbook.add_format({
     'bg_color': 'red',
-    'fort_color': 'white'
+    'font_color': 'white'
 })
 
 inserirDados = [
@@ -29,13 +29,13 @@ sheetDados.write('A1', "Células com valores >= 50 estão em verde e < 50 estão
 for linha, range in enumerate(inserirDados):
     sheetDados.write_row(linha + 2, 1, range)
 
-sheetDados.conditional_format('B4:E8', {'type': 'cell',
+sheetDados.conditional_format('B4:E7', {'type': 'cell',
                                         'criteria': '>=',
                                         'value': 50,
                                         'format': formatoMaior})
 
 
-sheetDados.conditional_format('B4:E8', {'type': 'cell',
+sheetDados.conditional_format('B4:E7', {'type': 'cell',
                                         'criteria': '<',
                                         'value': 50,
                                         'format': formatoMenor})
