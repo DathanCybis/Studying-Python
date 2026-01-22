7df9e0e855036eceb207e576d75d8c1f06955831
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-22 18:59:23 -0300

    Update ImagemNoExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/ImagemNoExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/ImagemNoExcel.py
@@ -1,12 +1,16 @@
 import xlsxwriter as xls
 import os
 
-nomeCaminho = 'C:\\..'
+nomeCaminho = 'C:\\...\\imagem.xlsx'
 
 workbook = xls.Workbook(nomeCaminho)
 sheetDados = workbook.add_worksheet("Dados")
 
 
-sheetDados.write("B3", "Imagem")
+sheetDados.write("B3", "Imagem logo YT")
 
-sheetDados.insert_image('B5', '')
+sheetDados.insert_image('B5', 'C:\\...\\.png')
+
+workbook.close()
+
+os.startfile(nomeCaminho)
