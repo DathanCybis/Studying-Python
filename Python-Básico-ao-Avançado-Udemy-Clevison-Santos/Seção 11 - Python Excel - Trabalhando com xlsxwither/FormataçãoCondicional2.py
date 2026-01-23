787e628bbf0ebc76efa1ca5654d6683e0cea5c5f
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-23 18:50:37 -0300

    Create FormataçãoCondicional2.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/FormataçãoCondicional2.py
@@ -0,0 +1,45 @@
+import xlsxwriter as xls
+import os
+
+nomeCaminho = 'C:\\...\\Condicional.xlsx'
+
+workbook = xls.Workbook(nomeCaminho)
+sheetDados = workbook.add_worksheet("Dados")
+
+
+formatoMaior = workbook.add_format({
+    'bg_color': 'green',
+    'font_color': 'white'
+})
+formatoMenor = workbook.add_format({
+    'bg_color': 'red',
+    'font_color': 'white'
+})
+
+inserirDados = [
+    ["Coluna 1", "Coluna 2", "Coluna 3", "Coluna 4"],
+    [34, 25, 64, 32],
+    [92, 72, 11, 3],
+    [37, 56, 87, 22],
+    [7, 49, 51, 99]
+]
+
+sheetDados.write('A1', "Células com valores >= 50 estão em verde e < 50 estão em vermelho")
+
+for linha, range in enumerate(inserirDados):
+    sheetDados.write_row(linha + 2, 1, range)
+
+sheetDados.conditional_format('B4:E7', {'type': 'cell',
+                                        'criteria': '>=',
+                                        'value': 50,
+                                        'format': formatoMaior})
+
+
+sheetDados.conditional_format('B4:E7', {'type': 'cell',
+                                        'criteria': '<',
+                                        'value': 50,
+                                        'format': formatoMenor})
+
+workbook.close()
+
+os.startfile(nomeCaminho)
