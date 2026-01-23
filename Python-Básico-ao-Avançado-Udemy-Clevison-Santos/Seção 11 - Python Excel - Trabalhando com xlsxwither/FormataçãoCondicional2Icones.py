1b6b4c776bea64cdb1bdba3925c02750510427f8
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-23 18:57:03 -0300

    Update FormataçãoCondicional2Icones.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/FormataçãoCondicional2Icones.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/FormataçãoCondicional2Icones.py
@@ -12,7 +12,8 @@ inserirDados = [
     [34, 25, 64, 32],
     [92, 72, 11, 3],
     [37, 56, 87, 22],
-    [7, 49, 51, 99]
+    [7, 49, 51, 99],
+    [3, 9, 29, 60]
 ]
 
 sheetDados.write('A1', "Formatação condicional com icones")
@@ -21,21 +22,21 @@ for linha, range in enumerate(inserirDados):
     sheetDados.write_row(linha + 2, 1, range)
 
 
-sheetDados.conditional_format('B4:E7', {'type': 'icon_set',
+sheetDados.conditional_format('B4:E4', {'type': 'icon_set',
                                         'icon_style': '3_traffic_lights'})
 
-sheetDados.conditional_format('B4:E7', {'type': 'icon_set',
+sheetDados.conditional_format('B5:E5', {'type': 'icon_set',
                                         'icon_style': '3_traffic_lights',
                                         'reverse_icons': True})
 
-sheetDados.conditional_format('B4:E7', {'type': 'icon_set',
+sheetDados.conditional_format('B6:E6', {'type': 'icon_set',
                                         'icon_style': '3_arrows'})
 
-sheetDados.conditional_format('B4:E7', {'type': 'icon_set',
+sheetDados.conditional_format('B7:E7', {'type': 'icon_set',
                                         'icon_style': '4_arrows'})
 
-sheetDados.conditional_format('B4:E7', {'type': 'icon_set',
-                                        'icon_style': '5_ratngs'})
+sheetDados.conditional_format('B8:E8', {'type': 'icon_set',
+                                        'icon_style': '5_ratings'})
 
 
 workbook.close()
