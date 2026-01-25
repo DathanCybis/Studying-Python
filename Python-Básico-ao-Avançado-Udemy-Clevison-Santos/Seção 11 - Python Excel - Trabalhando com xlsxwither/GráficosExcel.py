b37950c19cb04c360b38e608a8ee90ef90d2c458
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-24 23:46:48 -0300

    Create GráficosExcel.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 11 - Python Excel - Trabalhando com xlsxwither/GráficosExcel.py
@@ -0,0 +1,25 @@
+import xlsxwriter as xls
+import os
+
+nomeCaminho = 'C:\\...\\Gráficos.xlsx'
+
+workbook = xls.Workbook(nomeCaminho)
+sheetDados = workbook.add_worksheet("Dados")
+
+negrito = workbook.add_format({'bold': 1})
+
+titulos = ['Vendedores', 'Total Vendas']
+dadosTabela = [
+    ['Ana', 'Pedro', 'Allan', 'Francisco', 'Rosa', 'Amanda'],
+    [400, 300, 89, 34, 350, 120],
+]
+
+sheetDados.write_row('A1', titulos, negrito)
+sheetDados.write_column('A2', dadosTabela[0])
+sheetDados.write_column('B2', dadosTabela[1])
+
+
+
+workbook.close()
+
+os.startfile(nomeCaminho)
