e2dc123ad50658e70719ebb1bbee7e5310c0ae21
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-27 08:50:32 -0300

    Update DeletarLinhaColunaExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/DeletarLinhaColunaExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/DeletarLinhaColunaExcel.py
@@ -8,7 +8,9 @@ sheet_selecionada = planilha_aberta['Professor']
 
 sheet_selecionada.delete_rows(3)
 sheet_selecionada.delete_rows(3)
+sheet_selecionada.delete_rows(5)
 
+sheet_selecionada.delete_cols(2)
 
 planilha_aberta.save(filename=nomeCaminho)
 
