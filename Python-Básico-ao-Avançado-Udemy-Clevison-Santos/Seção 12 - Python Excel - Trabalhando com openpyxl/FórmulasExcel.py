64c8e2f619781f7334b1dd0ff2f5c71f7f0fd03e
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-30 07:55:44 -0300

    Update FórmulasExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/FórmulasExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/FórmulasExcel.py
@@ -13,6 +13,11 @@ sheet_selecionada["D3"] = "=A3-B3"
 sheet_selecionada["D4"] = "=A3*B3"
 sheet_selecionada["D5"] = "=A3/B3"
 
+sheet_selecionada["B12"] = "=MID(A12,1,3)"
+sheet_selecionada["C12"] = "=MID(A12,5,3)"
+sheet_selecionada["D12"] = "=MID(A12,9,3)"
+sheet_selecionada["E12"] = "=MID(A12,13,2)"
+
 planilha_aberta.save(filename=nomeCaminho)
 
 os.startfile(nomeCaminho)
