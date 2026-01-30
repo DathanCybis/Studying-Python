eb0d87756d27633cecf483bff2a1635f105fcf43
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-01-30 07:53:22 -0300

    Update FórmulasExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/FórmulasExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/FórmulasExcel.py
@@ -1,12 +1,13 @@
 from openpyxl import load_workbook
 import os
 
-nomeCaminho = 'C:\\...\\DeletarLinhaColunaExcel.xlsx'
+nomeCaminho = 'C:\\...\\Fórmulas.xlsx'
 planilha_aberta = load_workbook(filename=nomeCaminho)
 
 sheet_selecionada = planilha_aberta['Professor']
 
-
+sheet_selecionada["A6"] = "SUM(A2:A5)"
+sheet_selecionada["B6"] = "SUM(B2:B5)"
 
 planilha_aberta.save(filename=nomeCaminho)
 
