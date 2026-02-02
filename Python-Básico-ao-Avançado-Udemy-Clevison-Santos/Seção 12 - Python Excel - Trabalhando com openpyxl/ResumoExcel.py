d2184bfaf920c1ca3bded922fc5d061c7694c942
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-02 10:16:07 -0300

    Update ResumoExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/ResumoExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/ResumoExcel.py
@@ -15,6 +15,15 @@ for linha in range(2, len(sheet_selecionada['A'])+1):
     if sheet_selecionada['A%s' % linha].value == 'Amanda Martins':
         somarAmanda = somarAmanda + sheet_selecionada['C%s' % linha].value
     
+    elif sheet_selecionada['B%s' % linha].value == 'Eliane Moreira':
+        somarElaine = somarAmanda + sheet_selecionada['C%s' % linha].value
+
+    elif sheet_selecionada['B%s' % linha].value == 'Leonardo Almeida':
+        somarLeonardo = somarAmanda + sheet_selecionada['C%s' % linha].value
+
+    elif sheet_selecionada['B%s' % linha].value == 'Nicolas Pereira':
+        somarNicolas = somarAmanda + sheet_selecionada['C%s' % linha].value
+    
 
 planilha_aberta.save(filename=nomeCaminho)
 
