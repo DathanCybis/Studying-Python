0aa6ed7a6e606618f496dd422bff2824bfb390a3
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-03 09:22:18 -0300

    Update DadosExcel.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/DadosExcel.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 12 - Python Excel - Trabalhando com openpyxl/DadosExcel.py
@@ -5,13 +5,18 @@ import os
 nomeCaminho = 'C:\\...\\DadosExcel.xlsx'
 planilha_aberta = load_workbook(filename=nomeCaminho)
 
-sheet_selecionada = planilha_aberta['Professor']
+sheet_selecionada = planilha_aberta['Dados']
+
+
+novoArquivo = workbook()
+novaPlanilha = nomeCaminho.active
+
+for linha in range(1, len(sheet_selecionada['A']) + 1):
+    for coluna in range(1, 10):
+        novaPlanilha.cell(row=linha, column=coluna).value = sheet_selecionada.cell(row=linha, column=coluna).value
 
 
 
+novoArquivo.save(filename=caminhoNovaPlanilha)
 
-
-
-planilha_aberta.save(filename=nomeCaminho)
-
-os.startfile(nomeCaminho)
+os.startfile(caminhoNovaPlanilha)
