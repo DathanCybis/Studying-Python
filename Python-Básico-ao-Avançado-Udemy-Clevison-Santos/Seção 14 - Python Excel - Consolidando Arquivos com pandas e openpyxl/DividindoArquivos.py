54ff0ea7b5b86eb55569d2b2f7f1091b809882c0
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-09 09:36:46 -0300

    Create DividindoArquivos.py

---
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 14 - Python Excel - Consolidando Arquivos com pandas e openpyxl/DividindoArquivos.py
@@ -0,0 +1,47 @@
+from openpyxl import load_workbook
+import os
+
+nomeArquivo = "C:\\...\\...\\DividindoArquivos.xlsx"
+
+planilha_aberta = load_workbook(filename=nomeArquivo)
+
+sheet_selecionada = planilha_aberta['Dados']
+
+nomeNovo = ""
+totalLinha = len(sheet_selecionada['A']) + 1
+
+for linha in range(2, totalLinha):
+    nomeAtual = sheet_selecionada['A%s' % linha].value
+
+    if nomeNovo == nomeAtual:
+        linhaSheet = len(sheet_selecionada2['A'] + 1)
+        celulaColunaA = "A" + str(linhaSheet)
+        celulaColunaB = "B" + str(linhaSheet)
+        celulaColunaC = "C" + str(linhaSheet)
+
+        sheet_selecionada2[celulaColunaA] = sheet_selecionada['A%s' % linha].value
+        sheet_selecionada2[celulaColunaB] = sheet_selecionada['B%s' % linha].value
+        sheet_selecionada2[celulaColunaC] = sheet_selecionada['C%s' % linha].value
+
+    else:
+        sheet_resumo = planilha_aberta.create_sheet(title=nomeAtual)
+
+        sheet_selecionada2 = planilha_aberta[nomeAtual]
+
+        nomeAtual = sheet_selecionada['A%s' % linha].value
+
+        caminhoNovaPlanilha = "C:\\...\\...\\DividindoArquivos.xlsx"
+
+        sheet_selecionada2['A1'] = "Vendedor"
+        sheet_selecionada2['B1'] = "Produtos"
+        sheet_selecionada2['C1'] = "Vendas"
+
+        sheet_selecionada2['A2'] = sheet_selecionada['A%s' % linha].value
+        sheet_selecionada2['B2'] = sheet_selecionada['B%s' % linha].value
+        sheet_selecionada2['C2'] = sheet_selecionada['C%s' % linha].value
+
+        criandoNovoExcel.save(filename=)
+
+planilha_aberta.save(filename=nomeArquivo)
+
+os.startfile(nomeArquivo)
