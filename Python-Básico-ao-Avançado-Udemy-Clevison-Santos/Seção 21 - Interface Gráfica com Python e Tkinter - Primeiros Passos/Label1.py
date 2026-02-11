b2b5fed2cba7682fe222a35039bd222bc88b1112
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-11 09:39:53 -0300

    Update Label1.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 21 - Interface Gráfica com Python e Tkinter - Primeiros Passos/Label1.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 21 - Interface Gráfica com Python e Tkinter - Primeiros Passos/Label1.py
@@ -12,6 +12,17 @@ intrucao2 = Label(text="Interface Gráfica")
 
 intrucao2.pack()
 
+r1 = Label(janela, text = "FLAT", relief=FLAT)
+r2 = Label(janela, text = "RAISED", relief=RAISED)
+r3 = Label(janela, text = "SUNKEN", relief=SUNKEN)
+r4 = Label(janela, text = "GROOVE", relief=GROOVE)
+r5 = Label(janela, text = "RIDGE", relief=RIDGE)
+
+r1.pack()
+r2.pack()
+r3.pack()
+r4.pack()
+r5.pack()
 
 
 janela.mainloop() 
