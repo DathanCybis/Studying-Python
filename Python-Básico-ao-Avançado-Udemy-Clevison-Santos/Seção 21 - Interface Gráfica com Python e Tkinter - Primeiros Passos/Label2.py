f4306eaf7cf5411517b652d714d20afdf222c869
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-12 09:40:49 -0300

    Update Label2.py

--- Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 21 - Interface Gráfica com Python e Tkinter - Primeiros Passos/Label2.py
+++ Python-Básico-ao-Avançado-Udemy-Clevison-Santos/Seção 21 - Interface Gráfica com Python e Tkinter - Primeiros Passos/Label2.py
@@ -4,6 +4,14 @@ janela = Tk()
 
 janela.title("Interface Gráfica / Label2")
 
+texto = """Curso de Tkinter
+Aprendendo como criar
+Interface gráfica com
+Python
+"""
 
+formato = Label(janela,
+                font = "Arial 40 bold",
+                text=texto).pack()
 
 janela.mainloop() 
