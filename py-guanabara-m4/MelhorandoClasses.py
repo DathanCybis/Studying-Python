2d900fa838c73396f92851d4f2e937bbc67654c6
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-15 15:14:46 -0300

    Update MelhorandoClasses.py

--- py-guanabara-m4/MelhorandoClasses.py
+++ py-guanabara-m4/MelhorandoClasses.py
@@ -1,7 +1,7 @@
 class Gafanhoto:
-    def __init__(self):
-        self.nome = ""
-        self.idade = 0
+    def __init__(self, nome, idade):
+        self.nome = nome
+        self.idade = idade
 
 
     def aniversario(self):
@@ -12,9 +12,7 @@ class Gafanhoto:
         return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade"
     
 
-g1 = Gafanhoto()
-g1.nome = "Maria"
-g1.idade = 67
+g1 = Gafanhoto("Maria", 67)
 g1.aniversario()
 print(g1.mensagem())
 
