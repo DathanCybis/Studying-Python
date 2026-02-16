9bbfb19f8889617fbb6e0c223548f02066fac863
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-16 18:16:42 -0300

    Update MelhorandoClasses.py

--- py-guanabara-m4/MelhorandoClasses.py
+++ py-guanabara-m4/MelhorandoClasses.py
@@ -16,10 +16,14 @@ class Gafanhoto:
         return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade"
 
 
+    def __getstate__(self):
+        return f"Estado: nome = {self.nome} ; idade = {self.idade}"
+
+
 g1 = Gafanhoto("Maria", 67)
 g1.aniversario()
 print(g1)
-
-
+print(g1.__dict__)
+print(g1.__getstate__())
 #print(g1.__doc__)
 
