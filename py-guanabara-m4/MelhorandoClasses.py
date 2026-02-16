9bdbdbd3dbea6514535384c942d84137f22b000b
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-16 18:14:23 -0300

    Update MelhorandoClasses.py

--- py-guanabara-m4/MelhorandoClasses.py
+++ py-guanabara-m4/MelhorandoClasses.py
@@ -15,15 +15,11 @@ class Gafanhoto:
     def __str__(self):
         return f"{self.nome} é Gafanhoto(a) e tem {self.idade} anos de idade"
 
+
 g1 = Gafanhoto("Maria", 67)
 g1.aniversario()
 print(g1)
 
-g2 = Gafanhoto("Mauro", 53)
-g2.aniversario()
-print(g2)
 
-g3 = Gafanhoto()
-print(g3)
+#print(g1.__doc__)
 
-print(g1.__doc__)
