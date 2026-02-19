9fb269cccef99979eb794dd56af5a418f449c8f3
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-19 16:26:56 -0300

    Update BibliotecaRich.py

--- py-guanabara-m4/BibliotecaRich.py
+++ py-guanabara-m4/BibliotecaRich.py
@@ -1,8 +1,16 @@
 from rich import print
 from rich.panel import Panel
+from rich.table import Table
 
 print("Olá, [bold red]Mundo[/]! :earth_americas:" )
 
 caixa = Panel("[red]Painel de exemplo[/]", title="Mensagem", width=35)
 
 print(caixa)
+
+tabela = Table(title="Tabela de preços")
+
+tabela.add_column("Nome")
+tabela.add_column("Preço")
+
+print(tabela)
