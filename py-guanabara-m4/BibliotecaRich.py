d4ccada56cb205ef241f61ed37954b0eea06ab53
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-19 16:23:10 -0300

    Update BibliotecaRich.py

--- py-guanabara-m4/BibliotecaRich.py
+++ py-guanabara-m4/BibliotecaRich.py
@@ -3,6 +3,6 @@ from rich.panel import Panel
 
 print("Olá, [bold red]Mundo[/]! :earth_americas:" )
 
-caixa = Panel("[red]Painel de exemplo[/]")
+caixa = Panel("[red]Painel de exemplo[/]", title="Mensagem")
 
 print(caixa)
