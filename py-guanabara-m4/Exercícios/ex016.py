dbd9e862138831a4fdb0bef39b72182b0374b9dd
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-23 11:39:16 -0300

    Update ex016.py

--- py-guanabara-m4/Exercícios/ex016.py
+++ py-guanabara-m4/Exercícios/ex016.py
@@ -1,4 +1,12 @@
+class Funcionario:
+    def __init__(self, nome, setor, cargo):
+        self.nome = nome
+        self.setor = setor
+        self.cargo = cargo
 
+
+    def apresentacao(self):
+        return f"Olá, sou {self.nome} e sou {self.cargo} do setor de {self.setor}, da empresa Curso em Vídeo"
 
 
 
