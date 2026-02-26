731313babc64886ef466a81261f05d331a3026a0
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-26 12:09:25 -0300

    Update ex017.py

--- py-guanabara-m4/Exercícios/ex017.py
+++ py-guanabara-m4/Exercícios/ex017.py
@@ -1,15 +1,18 @@
+from rich import print
+from rich.panel import Panel
+
 class Produto:
-    def __int__(self, nome, preco):
+    def __init__(self, nome, preco):
         self.nome = nome
         self.preco = preco
 
 
     def etiqueta(self):
-        print()
+        etqNome = Panel(f"{self.nome} \n R${self.preco:,.2f}", title="Produto", width=55)
+        print(etqNome)
 
 
-
-p1 = Produto("iPhon 17 Pro Max", 25_000.85)
+p1 = Produto("iPhone 17 Pro Max", 25_000.85)
 p2 = Produto("Notebook Gamer", 8_000)
 
 p1.etiqueta()
