a53c0b0d20d1dbb4653fda84bbd8fce6789e3417
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-26 11:59:26 -0300

    Update ex017.py

--- py-guanabara-m4/Exercícios/ex017.py
+++ py-guanabara-m4/Exercícios/ex017.py
@@ -1,3 +1,10 @@
+class Produto:
+    def __int__(self, nome, preco):
+        self.nome = nome
+        self.preco = preco
+
+
+
 p1 = Produto("iPhon 17 Pro Max", 25_000.85)
 p2 = Produto("Notebook Gamer", 8_000)
 
