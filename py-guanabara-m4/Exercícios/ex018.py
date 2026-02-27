ebba1d27618d82c7bc06f2160dd052f473d8daed
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-27 15:26:04 -0300

    Update ex018.py

--- py-guanabara-m4/Exercícios/ex018.py
+++ py-guanabara-m4/Exercícios/ex018.py
@@ -10,6 +10,17 @@ class Churrasco:
         por_participante = 0.4
         kg_preco = 82.40
         recomendado = 6.0
+        print(f"Analisando [red]{self.titulo}[/] com [blue]{self.quant} convidados[/]")
+        print(f"Cada participante comerá [yellow]{por_participante}Kg[/] e cada Kg custa [green]R${kg_preco:.2f}[/]")
+        print(f"Recomendo comprar [yellow]{recomendado:.3f}Kg[/] de carne")
+        custo_total = por_participante * self.quant
+        custo_total *= kg_preco
+        
+        print(f"O custo total será de [green]R${custo_total:.2f}[/]")
+        cada_pessoa = custo_total / self.quant
+        print(f"Cada pessoa pagará [green]R${cada_pessoa}[/] para participar.")
+        
+
 
 c1 = Churrasco("Churras dos Amigos", 15)
 c1.analisar()
