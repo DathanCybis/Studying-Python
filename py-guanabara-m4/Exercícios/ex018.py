22f05eeba8e46251d72699feb8a283892896a51b
DathanCybis <100447885+DathanCybis@users.noreply.github.com>
2026-02-27 15:25:37 -0300

    Update ex018.py

--- py-guanabara-m4/Exercícios/ex018.py
+++ py-guanabara-m4/Exercícios/ex018.py
@@ -1,2 +1,10 @@
+from rich import print
+
+class Churrasco:
+    def __init__(self, titulo, quant):
+        self.titulo = titulo
+        self.quant = quant
+
+
 c1 = Churrasco("Churras dos Amigos", 15)
 c1.analisar()
